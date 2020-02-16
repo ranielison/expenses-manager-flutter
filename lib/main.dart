02560742d325a448b2155d042dbe9efb09dd06db
Ranielison <ranielisonsoares2@gmail.com>
2020-02-16 16:04:59 -0300

    Criando o modelo transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -13,6 +14,21 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
