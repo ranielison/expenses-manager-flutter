3679f8c1e9aa171a6c1bd1353c0cc3509e69a9ed
Ranielison <ranielisonsoares2@gmail.com>
2020-02-16 19:25:02 -0300

    Resolvendo um erro no modal

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
     showModalBottomSheet(
       context: context,
       builder: (_) {
-        return TransactionForm(null);
+        return TransactionForm(_addTransaction);
       },
     );
   }
