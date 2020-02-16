87fc42e19efe79e5b32349bc6edfb612015643ae
Ranielison <ranielisonsoares2@gmail.com>
2020-02-16 16:29:38 -0300

    Continuando estilo do item despesa

--- lib/main.dart
+++ lib/main.dart
@@ -69,9 +69,19 @@ class MyHomePage extends StatelessWidget {
                     ),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tr.title),
-                      Text(tr.date.toString())
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      )
                     ],
                   )
                 ],
