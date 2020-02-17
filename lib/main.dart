0835271055e4a0cb3f7644ef58ea0ef810831607
Ranielison <ranielisonsoares2@gmail.com>
2020-02-16 22:37:01 -0300

    Exibindo imagem quando lista esta vazia

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
+  final List<Transaction> _transactions = [
+    /*Transaction(
       id: 't1',
       title: 'Novo Tênis de corrida',
       value: 310.76,
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Conta de luz',
       value: 211.30,
       date: DateTime.now(),
-    ),
+    ),*/
   ];
 
   _addTransaction(String title, double value) {
