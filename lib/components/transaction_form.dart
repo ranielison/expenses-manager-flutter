d28c2a826cd4022b0c65723934b093fc720f8930
Ranielison <ranielisonsoares2@gmail.com>
2020-02-18 22:53:32 -0300

    Colocando data na transação

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
-  final Function(String, double) onSubmit;
+  final Function(String, double, DateTime) onSubmit;
 
   TransactionForm(this.onSubmit);
 
@@ -14,15 +14,15 @@ class _TransactionFormState extends State<TransactionForm> {
   final _titleController = TextEditingController();
   final _valueController = TextEditingController();
 
-  DateTime _selectedDate;
+  DateTime _selectedDate = DateTime.now();
 
   _submitForm() {
     final title = _titleController.text;
     final value = double.parse(_valueController.text) ?? 0.0;
 
-    if (title.isEmpty || value <= 0) return;
+    if (title.isEmpty || value <= 0 || _selectedDate == null) return;
 
-    widget.onSubmit(title, value);
+    widget.onSubmit(title, value, _selectedDate);
   }
 
   _showDatePicker() {
--- lib/main.dart
+++ lib/main.dart
@@ -39,31 +39,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 3)),
-    ),
-    Transaction(
-        id: 't1',
-        title: 'Novo Tênis de corrida',
-        value: 310.76,
-        date: DateTime.now().subtract(Duration(days: 4))),
-    Transaction(
-      id: 't3',
-      title: 'Cartão de crédito',
-      value: 10011.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Lanche',
-      value: 11.30,
-      date: DateTime.now(),
-    ),
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -73,12 +49,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  _addTransaction(String title, double value) {
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now(),
+      date: date,
     );
 
     setState(() {
